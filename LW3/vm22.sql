921801e6574ebb8d2e22568632191c248df0f2a2
vilo4kaaa <vika.masyuk@gmail.com>
2022-07-13 01:02:51 +0300

    refactor: remove empty rows

--- LW3/vm22.sql
+++ LW3/vm22.sql
@@ -1,14 +1,11 @@
+SELECT segment_name, segment_type FROM user_segments;
 PURGE RECYCLEBIN;
 
 CREATE TABLE t ( x INT PRIMARY KEY, y CLOB, z BLOB); 
-
 SELECT segment_name, segment_type FROM user_segments;
-
 DROP TABLE T;
 
 CREATE TABLE t ( x INT PRIMARY KEY, y CLOB, z BLOB) SEGMENT CREATION IMMEDIATE; 
-
 SELECT segment_name, segment_type FROM user_segments;
-
 SELECT DBMS_METADATA.GET_DDL('TABLE','T') FROM dual;
 
