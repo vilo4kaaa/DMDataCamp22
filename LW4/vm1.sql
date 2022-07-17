eb9c5cf7d58d3eee979d2c06ff9824b4f2060073
vilo4kaaa <vika.masyuk@gmail.com>
2022-07-17 12:35:50 +0300

    feat: add vm1

---
+++ LW4/vm1.sql
@@ -0,0 +1,48 @@
+CREATE TABLE t2 AS 
+ SELECT TRUNC( rownum / 100 ) id, rpad( rownum, 100 ) t_pad 
+   FROM dual 
+CONNECT BY rownum < 100000; 
+
+CREATE INDEX t2_idx1 ON t2 
+  ( id ); 
+  
+SELECT BLOCKS FROM user_segments WHERE segment_name = 'T2';
+SELECT COUNT(DISTINCT (dbms_rowid.rowid_block_number(rowid))) block_ct FROM t2 ;
+SET autotrace ON;
+SELECT COUNT( * ) 
+   FROM t2 ;
+SET autotrace OFF;
+
+----------------------------
+DELETE FROM t2;
+
+SELECT BLOCKS FROM user_segments WHERE segment_name = 'T2';
+SELECT COUNT(DISTINCT (dbms_rowid.rowid_block_number(rowid))) block_ct FROM t2 ;
+SET autotrace ON;
+SELECT COUNT( * ) 
+   FROM t2 ;
+SET autotrace OFF;
+   
+----------------------------  
+INSERT INTO t2 ( ID, T_PAD ) 
+  VALUES (  1,'1' ); 
+COMMIT; 
+
+SELECT BLOCKS FROM user_segments WHERE segment_name = 'T2';
+SELECT COUNT(DISTINCT (dbms_rowid.rowid_block_number(rowid))) block_ct FROM t2 ;
+
+SET autotrace ON;
+SELECT COUNT( * ) 
+   FROM t2 ;
+   
+----------------------------  
+TRUNCATE TABLE t2;  
+
+SELECT BLOCKS FROM user_segments WHERE segment_name = 'T2';
+SELECT COUNT(DISTINCT (dbms_rowid.rowid_block_number(rowid))) block_ct FROM t2 ;
+
+SET autotrace ON;
+SELECT COUNT( * ) 
+   FROM t2 ;
+   
+DROP TABLE t2;
