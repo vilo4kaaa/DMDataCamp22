30a3c5be766f3b39dd5d9e6df137984054747f33
vilo4kaaa <vika.masyuk@gmail.com>
2022-07-19 13:42:19 +0300

    feat: init external directory

---
+++ LW6/lab_scripts/task01-Open/System/directories/init_directories.sql
@@ -0,0 +1,8 @@
+--Create Directory with path to External References files storage
+
+CREATE OR REPLACE DIRECTORY ext_references
+AS
+  '/mnt/ext_references/';
+   
+GRANT READ ON DIRECTORY ext_references TO u_dw_ext_references;
+GRANT WRITE ON DIRECTORY ext_references TO u_dw_ext_references;
