96bd1dee9f90ea7360d2bc2d702485a05e13dd00
vilo4kaaa <vika.masyuk@gmail.com>
2022-07-19 14:28:25 +0300

    refactor: t_localizations

--- LW6/lab_scripts/task01-Open/u_dw_references/tables/t_localizations/t_localizations.sql
+++ LW6/lab_scripts/task01-Open/u_dw_references/tables/t_localizations/t_localizations.sql
@@ -1,15 +1,17 @@
-alter table u_dw_references.lc_lng_scopes
-   drop constraint FK_LOC2LNG_SCOPES;
+/*
+ALTER TABLE u_dw_references.lc_lng_scopes
+   DROP CONSTRAINT FK_LOC2LNG_SCOPES;
 
-alter table u_dw_references.lc_lng_types
-   drop constraint FK_LOC2LNG_TYPES;
+ALTER TABLE u_dw_references.lc_lng_types
+   DROP CONSTRAINT FK_LOC2LNG_TYPES;
 
-drop table u_dw_references.t_localizations cascade constraints;
+DROP TABLE u_dw_references.t_localizations CASCADE CONSTRAINTS;
+*/
 
---==============================================================
+----------------------------------------------------------------
 -- Table: t_localizations                                       
---==============================================================
-create table u_dw_references.t_localizations 
+
+CREATE TABLE u_dw_references.t_localizations 
 (
    localization_id      NUMBER(22,0)         not null,
    localization_code    VARCHAR2(5 CHAR)     not null,
@@ -18,31 +20,31 @@ create table u_dw_references.t_localizations
    lng_id               NUMBER(22,0),
    contry_id            NUMBER(22,0),
    is_default           INTEGER,
-   constraint PK_T_LOCALIZATIONS primary key (localization_id)
-         using index tablespace TS_REFERENCES_IDX_01
+   CONSTRAINT PK_T_LOCALIZATIONS PRIMARY KEY (localization_id)
+         USING INDEX TABLESPACE TS_REFERENCES_IDX_01
 )
-tablespace TS_REFERENCES_DATA_01;
+TABLESPACE TS_REFERENCES_DATA_01;
 
-comment on column u_dw_references.t_localizations.localization_id is
+COMMENT ON COLUMN u_dw_references.t_localizations.localization_id is
 'Identificator of Supported References Languages';
 
-comment on column u_dw_references.t_localizations.localization_code is
+COMMENT ON COLUMN u_dw_references.t_localizations.localization_code is
 'Code of Supported References Languages';
 
-comment on column u_dw_references.t_localizations.localization_desc is
+COMMENT ON COLUMN u_dw_references.t_localizations.localization_desc is
 'Name of Supported References Languages';
 
-comment on column u_dw_references.t_localizations.localization_desc_ens is
+COMMENT ON COLUMN u_dw_references.t_localizations.localization_desc_ens is
 'Endonym Name of  Supported References Languages';
 
-comment on column u_dw_references.t_localizations.lng_id is
+COMMENT ON COLUMN u_dw_references.t_localizations.lng_id is
 'Disabled - FK for Language_Id - Post Mapped by Load PKG';
 
-comment on column u_dw_references.t_localizations.contry_id is
+COMMENT ON COLUMN u_dw_references.t_localizations.contry_id is
 'Disabled - FK for Country_Id - Post Mapped by Load PKG';
 
-comment on column u_dw_references.t_localizations.is_default is
+COMMENT ON COLUMN u_dw_references.t_localizations.is_default is
 'Default Language for all Application and Members on DataBase';
 
-alter table u_dw_references.t_localizations
-   add constraint CHK_T_LOCALIZATIONS_IS_DEFAULT check (is_default is null or (is_default in (1,0)));
+ALTER TABLE u_dw_references.t_localizations
+   ADD CONSTRAINT CHK_T_LOCALIZATIONS_IS_DEFAULT CHECK (is_default IS NULL OR (is_default IN (1,0)));
