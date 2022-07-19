f9a8632b090ca4ded73947f63d484f2ac893b154
vilo4kaaa <vika.masyuk@gmail.com>
2022-07-19 14:22:06 +0300

    refactor: t_lng_links

--- LW6/lab_scripts/task01-Open/u_dw_references/tables/t_lng_links/t_lng_links.sql
+++ LW6/lab_scripts/task01-Open/u_dw_references/tables/t_lng_links/t_lng_links.sql
@@ -1,45 +1,43 @@
-alter table u_dw_references.t_lng_links
-   drop constraint FK_T_LANGUAGES2T_LNG_LINKS_C;
+/*
+ALTER TABLE u_dw_references.t_lng_links
+   DROP CONSTRAINT FK_T_LANGUAGES2T_LNG_LINKS_C;
 
-alter table u_dw_references.t_lng_links
-   drop constraint FK_T_LANGUAGES2T_LNG_LINKS_P;
+ALTER TABLE u_dw_references.t_lng_links
+   DROP CONSTRAINT FK_T_LANGUAGES2T_LNG_LINKS_P;
 
-drop table u_dw_references.t_lng_links cascade constraints;
+DROP table u_dw_references.t_lng_links CASCADE CONSTRAINTS;
 
---==============================================================
+*/
+
+---------------------------------------------------------------
 -- Table: t_lng_links                                           
---==============================================================
-create table u_dw_references.t_lng_links 
+
+CREATE TABLE u_dw_references.t_lng_links 
 (
    parent_lng_id        NUMBER(22,0)         not null,
    child_lng_id         NUMBER(22,0)         not null,
    link_type_id         NUMBER(3,0)          not null,
-   constraint PK_T_LNG_LINKS primary key (parent_lng_id, child_lng_id, link_type_id)
-         using index
-       local
-       tablespace TS_REFERENCES_IDX_01
+   CONSTRAINT PK_T_LNG_LINKS PRIMARY KEY (parent_lng_id, child_lng_id, link_type_id)
+   USING INDEX LOCAL TABLESPACE TS_REFERENCES_IDX_01
 )
-tablespace TS_REFERENCES_DATA_01
- partition by list
+TABLESPACE TS_REFERENCES_DATA_01
+ PARTITION BY LIST
  (link_type_id)
- (partition
-         p_Macro2Individ
-        values (1)
-         nocompress);
+ (PARTITION p_Macro2Individ VALUES (1) NOCOMPRESS);
 
-comment on column u_dw_references.t_lng_links.parent_lng_id is
+COMMENT ON COLUMN u_dw_references.t_lng_links.parent_lng_id is
 'Link: Paranet Object - Languages T_LANGUAGES';
 
-comment on column u_dw_references.t_lng_links.child_lng_id is
+COMMENT ON COLUMN u_dw_references.t_lng_links.child_lng_id is
 'Link: Child Object - Languages T_LANGUAGES';
 
-comment on column u_dw_references.t_lng_links.link_type_id is
+COMMENT ON COLUMN u_dw_references.t_lng_links.link_type_id is
 'Link Type: 1 - Macrolanguages to Individual Languages';
 
-alter table u_dw_references.t_lng_links
-   add constraint FK_T_LANGUAGES2T_LNG_LINKS_C foreign key (child_lng_id)
-      references u_dw_references.t_languages (lng_id);
+ALTER TABLE u_dw_references.t_lng_links
+   ADD CONSTRAINT FK_T_LANGUAGES2T_LNG_LINKS_C FOREIGN KEY (child_lng_id)
+      REFERENCES u_dw_references.t_languages (lng_id);
 
-alter table u_dw_references.t_lng_links
-   add constraint FK_T_LANGUAGES2T_LNG_LINKS_P foreign key (parent_lng_id)
-      references u_dw_references.t_languages (lng_id);
+ALTER TABLE u_dw_references.t_lng_links
+   ADD CONSTRAINT FK_T_LANGUAGES2T_LNG_LINKS_P FOREIGN KEY (parent_lng_id)
+      REFERENCES u_dw_references.t_languages (lng_id);
