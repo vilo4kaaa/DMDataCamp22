119157ce55bd0d387c929f29b0bb04af95c5283c
vilo4kaaa <vika.masyuk@gmail.com>
2022-08-11 15:07:07 +0300

    feat: add packages for movies

--- BUSINESS TASK/cls_level/packages/pkg_etl_cls_movies_impl.sql
+++ BUSINESS TASK/cls_level/packages/pkg_etl_cls_movies_impl.sql
@@ -3,19 +3,18 @@ AS
   PROCEDURE load_cls_movies
    AS
 
-      CURSOR cursor_cls_moviies
+      CURSOR cursor_cls_movies
       IS
-         SELECT DISTINCT MOVIE_NAME, MOVIE_YEAR, MOVIE_GENRE, MOVIE_LEVEL1, MOVIE_LEVEL2, MOVIE_LEVEL3, MOVIE_DURATION
-           FROM SA_CURRENCIES_DATA.SA_T_MOVIES
+         SELECT DISTINCT MOVIE_NAME, MOVIE_YEAR, MOVIE_LEVEL1, MOVIE_LEVEL2, MOVIE_LEVEL3, MOVIE_DURATION
+           FROM SA_EXT_REF_DATA.SA_T_MOVIE
            WHERE MOVIE_NAME IS NOT NULL
            AND MOVIE_YEAR IS NOT NULL;
    BEGIN
-   EXECUTE IMMEDIATE 'TRUNCATE TABLE CLS_T_MOVIES';
+   EXECUTE IMMEDIATE 'TRUNCATE TABLE CLS_T_MOVIE';
       FOR i IN cursor_cls_movies LOOP
-         INSERT INTO DW_CL.CLS_T_MOVIES( 
+         INSERT INTO DW_CL.CLS_T_MOVIE( 
                        MOVIE_NAME,
                        MOVIE_YEAR,
-                       MOVIE_GENRE,
                        MOVIE_LEVEL1,
                        MOVIE_LEVEL2,
                        MOVIE_LEVEL3,
@@ -23,7 +22,6 @@ AS
               VALUES ( 
                 i.MOVIE_NAME,
                 i.MOVIE_YEAR,
-                i.MOVIE_GENRE,
                 i.MOVIE_LEVEL1,
                 i.MOVIE_LEVEL2,
                 i.MOVIE_LEVEL3,
@@ -37,4 +35,4 @@ END pkg_etl_cls_movies;
 alter session set current_schema = DW_CL;
 EXEC pkg_etl_cls_movies.load_cls_movies;
 
-SELECT * FROM cls_t_movies;
+SELECT * FROM cls_t_movie;
