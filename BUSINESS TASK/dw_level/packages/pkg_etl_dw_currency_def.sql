5860a03e712bd9a508b28bf0019e1f99c4794a50
vilo4kaaa <vika.masyuk@gmail.com>
2022-08-15 12:44:53 +0300

    feat: add refactor pkg with ref cursor

--- BUSINESS TASK/dw_level/packages/pkg_etl_dw_currency_def.sql
+++ BUSINESS TASK/dw_level/packages/pkg_etl_dw_currency_def.sql
@@ -3,5 +3,7 @@ alter session set current_schema = DW_DATA;
 CREATE OR REPLACE PACKAGE pkg_etl_dw_currency
 AS  
     PROCEDURE load_dw_currency;
+    PROCEDURE load_dw_currency_ref_cursor;
+    PROCEDURE load_dw_currency_num_cursor;
 END pkg_etl_dw_currency;
 /
--- BUSINESS TASK/dw_level/packages/pkg_etl_dw_currency_impl.sql
+++ BUSINESS TASK/dw_level/packages/pkg_etl_dw_currency_impl.sql
@@ -58,18 +58,75 @@ AS
          WHERE currency_id = currency_id(i);
 	
 	         COMMIT;
-          
-              
        END IF;
- 
     END LOOP;
 	  
 	END;
    END load_dw_currency;
+   
+   
+   
+PROCEDURE load_dw_currency_ref_cursor
+   AS
+     BEGIN 
+     DECLARE 
+       cursor_id NUMBER (25);
+       cur_count NUMBER (38);
+       quary_cur VARCHAR2(2000);
+       TYPE ref_crsr IS REF CURSOR;
+       ref_cursor ref_crsr;
+       TYPE type_rec IS RECORD
+	   (    CURRENCY_NAME VARCHAR(20),
+            CURRENCY_COUNTRY_CODE VARCHAR2(10),
+            EXCHANGE_RATE  DECIMAL(5,2),
+            CURRENCY_ID NUMBER);
+       one_record type_rec;
+       
+     BEGIN 
+     EXECUTE IMMEDIATE 'TRUNCATE TABLE DW_DATA.dim_currency CASCADE';
+     quary_cur:= 'SELECT CURRENCY_NAME, CURRENCY_NAME, EXCHANGE_RATE, CURRENCY_ID FROM
+                        (SELECT source.CURRENCY_NAME AS CURRENCY_NAME,
+                        source.CURRENCY_COUNTRY_CODE AS CURRENCY_COUNTRY_CODE, 
+                         source. EXCHANGE_RATE AS  EXCHANGE_RATE,
+                         stage.CURRENCY_ID AS CURRENCY_ID
+                  FROM DW_CL.CLS_T_CURRENCY source
+                  LEFT JOIN DW_DATA.dim_currency stage
+                  ON (source.CURRENCY_NAME = stage.CURRENCY_NAME))';
+                  
+      cursor_id:=DBMS_SQL.open_cursor;
+      
+      DBMS_SQL.PARSE(cursor_id, quary_cur, DBMS_SQL.NATIVE);
+      
+      cur_count:= DBMS_SQL.EXECUTE(cursor_id);
+      
+      ref_cursor:= DBMS_SQL.TO_REFCURSOR(cursor_id);
+      
+      LOOP
+      FETCH ref_cursor INTO one_record;
+      EXIT WHEN ref_cursor%NOTFOUND;
+      IF (one_record.CURRENCY_ID IS NULL) THEN
+                INSERT INTO DW_DATA.dim_currency(   CURRENCY_ID,
+                                                    CURRENCY_NAME,
+                                                    CURRENCY_COUNTRY_CODE,
+                                                    EXCHANGE_RATE )
+                VALUES (SEQ_CURRENCY.NEXTVAL,
+                        one_record.CURRENCY_NAME, 
+                        one_record.CURRENCY_NAME,
+                        one_record.EXCHANGE_RATE
+                        ); 
+      END IF;
+      END LOOP;
+    COMMIT;
+    END;
+   END load_dw_currency_ref_cursor;
+   
 END pkg_etl_dw_currency;
 
 
-EXEC pkg_etl_dw_currency.load_dw_currency;
+--EXEC pkg_etl_dw_currency.load_dw_currency;
+--EXEC pkg_etl_dw_currency.load_dw_currency_ref_cursor;
+EXEC pkg_etl_dw_currency.load_dw_currency_num_cursor;
 SELECT * FROM dim_currency;
 
+DROP TABLE dim_currency CASCADE CONSTRAINTS;
 
